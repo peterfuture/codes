bd697c3d59e3e43f53e9b0d4af76b2f67220b81d
peterfuture <peter_future@outlook.com>
2014-11-20 08:31:45 +0800

    [OPENSSL] update test codes

--- src/openssl_test.cpp
+++ src/openssl_test.cpp
@@ -59,7 +59,69 @@ int openssl_test()
 	rsa_key.reset();
 	auto l = BIO_get_mem_data(shared_keyfile.get(), &outbuf);
 	keyfilecontent.assign(outbuf, l);
-	shared_keyfile.reset();
+	shared_keyfile.reset();	
+	//std::cout << "private_key:" << keyfilecontent << " cert_content:" << certfilecontent;
+
+	//Step1 - Client Generate random g p pub_key
+	unsigned char client_content[512];
+	DH *dh_client = DH_new();
+	DH_generate_parameters_ex(dh_client,64,DH_GENERATOR_5,NULL);
+	DH_generate_key(dh_client);
+
+	// 把 g,p, pubkey 传过去
+	std::string client_random_g, client_random_p, client_pub_key;
+	client_random_g.assign(reinterpret_cast<const char*>(client_content), BN_bn2bin(dh_client->g, client_content));
+	client_random_p.assign(reinterpret_cast<const char*>(client_content), BN_bn2bin(dh_client->p, client_content));
+	client_pub_key.assign(reinterpret_cast<const char*>(client_content), BN_bn2bin(dh_client->pub_key, client_content));
+	std::cout << "client public key:" << client_pub_key << std::endl;
+	
+	// Step 2 - Server Generate random number
+	DH* dh_server = DH_new();
+	unsigned char bin_key[512] = { 0 };
+	std::string server_pubkey;
+
+	// 生成随机数然后返回 m_dh->p ，让客户端去算共享密钥.
+	DH_generate_parameters_ex(dh_server, 64, DH_GENERATOR_5, NULL);
+	dh_server->g = BN_bin2bn((const unsigned char *)client_random_g.data(), static_cast<long>(client_random_g.length()), dh_server->g);
+	dh_server->p = BN_bin2bn((const unsigned char *)client_random_p.data(), static_cast<long>(client_random_p.length()), dh_server->p);
+
+	DH_generate_key(dh_server);
+	server_pubkey.assign(reinterpret_cast<const char*>(bin_key), BN_bn2bin(dh_server->pub_key, bin_key));
+	
+	// store client public key
+	std::vector<uint8_t> server_shared_key;
+	server_shared_key.resize(DH_size(dh_server));
+	BIGNUM* server_client_pubkey = BN_bin2bn((const unsigned char *)client_pub_key.data(), static_cast<long>(client_pub_key.length()), NULL);
+	DH_compute_key(&server_shared_key[0], server_client_pubkey, dh_server);
+	BN_free(server_client_pubkey);
+	DH_free(dh_server);
+	
+	std::string server_key;
+	char buf[16] = { 0 };
+	for (int i = 0; i < server_shared_key.size(); ++i)
+	{
+		sprintf(buf, "%x%x", (server_shared_key[i] >> 4) & 0xf, server_shared_key[i] & 0xf);
+		server_key += buf;
+	}
+
+	std::cout << "server_key: " << server_key << std::endl;
+	
+	//step3 - Client calc server key
+	auto client_server_pubkey = BN_bin2bn((const unsigned char *) server_pubkey.data(), server_pubkey.length(), NULL);
+	std::vector<unsigned char> client_shared_key;
+	client_shared_key.resize(DH_size(dh_client));
+	// 密钥就算出来啦！
+	DH_compute_key(&client_shared_key[0], client_server_pubkey, dh_client);
+	BN_free(client_server_pubkey);
+
+    std::printf("key = 0x");
+    for (int i=0; i<DH_size(dh_client); ++i)
+	{
+        std::printf("%x%x", (client_shared_key[i] >> 4) & 0xf, client_shared_key[i] & 0xf);
+    }
+    std::printf("\n");
+	DH_free(dh_client);
+	
 	
 	std::cout<< "openssl test end" << std::endl;
 	return 0;
