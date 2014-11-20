17e6343ea5fa5611bd94b773dade79886a32d550
peter future <peter_future@outlook.com>
2014-11-20 20:05:06 +0800

    [OPENSSL]update openssl test codes

--- src/openssl_test.cpp
+++ src/openssl_test.cpp
@@ -11,6 +11,96 @@ namespace fs = boost::filesystem;
 
 using namespace std;
 
+//== DH
+DH *dh_client;
+DH *dh_server;
+std::string random_g, random_p;
+std::string server_pubkey, client_pubkey;
+std::vector<uint8_t> client_shared_key;
+std::vector<uint8_t> server_shared_key;
+
+//== CA
+boost::shared_ptr<RSA> rsa_key;
+boost::shared_ptr<X509> x509_cert;
+std::string keyfilecontent, certfilecontent;
+
+//step1 Client Generate HD P-G-PUBKEY
+int dh_generate_clientkey()
+{
+	//Generate random g p pub_key
+	unsigned char client_content[512] = {0};
+	dh_client = DH_new();
+	DH_generate_parameters_ex(dh_client,64,DH_GENERATOR_5,NULL);
+	DH_generate_key(dh_client);
+
+	random_g.assign(reinterpret_cast<const char*>(client_content), BN_bn2bin(dh_client->g, client_content));
+	random_p.assign(reinterpret_cast<const char*>(client_content), BN_bn2bin(dh_client->p, client_content));
+	client_pubkey.assign(reinterpret_cast<const char*>(client_content), BN_bn2bin(dh_client->pub_key, client_content));
+
+	return 0;
+}
+
+//step2 Server Generate shared Key with Client's P-G-KEY
+//And send pub key to client
+int dh_generate_serverkey()
+{	
+	unsigned char server_content[512] = { 0 };
+	dh_server = DH_new();
+	DH_generate_parameters_ex(dh_server, 64, DH_GENERATOR_5, NULL);
+
+	dh_server->g = BN_bin2bn((const unsigned char *)random_g.data(), static_cast<long>(random_g.length()), dh_server->g);
+	dh_server->p = BN_bin2bn((const unsigned char *)random_p.data(), static_cast<long>(random_p.length()), dh_server->p);
+	DH_generate_key(dh_server);
+	server_pubkey.assign(reinterpret_cast<const char*>(server_content), BN_bn2bin(dh_server->pub_key, server_content));
+	
+	// store client public key
+	server_shared_key.resize(DH_size(dh_server));
+	BIGNUM* server_client_pubkey = BN_bin2bn((const unsigned char *)client_pubkey.data(), static_cast<long>(client_pubkey.length()), NULL);
+	DH_compute_key(&server_shared_key[0], server_client_pubkey, dh_server);
+	BN_free(server_client_pubkey);
+	DH_free(dh_server);
+	
+	std::string server_key;
+	char buf[16] = { 0 };
+	for (int i = 0; i < server_shared_key.size(); ++i)
+	{
+		sprintf(buf, "%x%x", (server_shared_key[i] >> 4) & 0xf, server_shared_key[i] & 0xf);
+		server_key += buf;
+	}
+	
+	std::cout << "Generate server shared key:" << server_key << std::endl;
+	return 0;
+}
+
+//step3 client generate sharedkey with server pubkey
+int dh_generate_client_shared_key()
+{
+	client_shared_key.resize(DH_size(dh_client));
+	BIGNUM* client_server_pubkey = BN_bin2bn((const unsigned char *)server_pubkey.data(), static_cast<long>(server_pubkey.length()), NULL);
+	DH_compute_key(&client_shared_key[0], client_server_pubkey, dh_client);
+	BN_free(client_server_pubkey);
+	DH_free(dh_client);
+
+	std::string client_key;
+	char buf[16] = { 0 };
+    for (int i=0; i< client_shared_key.size(); ++i)
+	{
+		sprintf(buf, "%x%x", (client_shared_key[i] >> 4) & 0xf, client_shared_key[i] & 0xf);
+		client_key += buf;
+    }
+    
+	std::cout << "Generate client shared key:" << client_key << std::endl;
+	return 0;
+}
+
+int dh_test()
+{
+	dh_generate_clientkey();
+	dh_generate_serverkey();
+	dh_generate_client_shared_key();
+	return 0;
+}
+
 static int pass_cb(char *buf, int size, int rwflag, char *u)
 {
 	int len;
@@ -31,97 +121,128 @@ static int pass_cb(char *buf, int size, int rwflag, char *u)
 	return len;
 }
 
-int openssl_test()
-{	
-	OpenSSL_add_all_algorithms();
-	std::string keyfilecontent, keyfilecontent_decrypted, certfilecontent;
-	
+int dump_ca_info()
+{
+	unsigned char * CN = NULL;
+	auto cert_name = X509_get_subject_name(x509_cert.get());
+	auto cert_entry = X509_NAME_get_entry(cert_name, X509_NAME_get_index_by_NID(cert_name, NID_commonName, 0));
+	ASN1_STRING *entryData = X509_NAME_ENTRY_get_data(cert_entry);
+	auto strlengh = ASN1_STRING_to_UTF8(&CN, entryData);
+	std::printf("%s\n",CN);
+	OPENSSL_free(CN);
+}
+
+int ca_load()
+{
 	fs::path key("./test.key");
 	fs::path cert("./test.crt");
-
+	
 	std::ifstream keyfile(key.string().c_str(), std::ios_base::binary | std::ios_base::in);
 	std::ifstream certfile(cert.string().c_str(), std::ios_base::binary | std::ios_base::in);
+	
 	keyfilecontent.resize(fs::file_size(key));
 	certfilecontent.resize(fs::file_size(cert));
 	keyfile.read(&keyfilecontent[0], fs::file_size(key));
 	certfile.read(&certfilecontent[0], fs::file_size(cert));
 	
-	// 这里通过读取然后写回的方式预先将私钥的密码去除
 	boost::shared_ptr<BIO> shared_keyfile(BIO_new_mem_buf(&keyfilecontent[0], keyfilecontent.length()), BIO_free);
-	boost::shared_ptr<RSA> rsa_key(
+	boost::shared_ptr<BIO> shared_certfile(BIO_new_mem_buf(&certfilecontent[0], certfilecontent.length()), BIO_free);
+
+	boost::shared_ptr<RSA> rsa_key2(
 		PEM_read_bio_RSAPrivateKey(shared_keyfile.get(), 0, (pem_password_cb*)pass_cb,(void*) key.c_str()),
 		RSA_free
 	);
-
 	shared_keyfile.reset(BIO_new(BIO_s_mem()), BIO_free);
-	char *outbuf = 0;
-	PEM_write_bio_RSAPrivateKey(shared_keyfile.get(),rsa_key.get(), 0, 0, 0, 0, 0);
-	rsa_key.reset();
-	auto l = BIO_get_mem_data(shared_keyfile.get(), &outbuf);
-	keyfilecontent.assign(outbuf, l);
-	shared_keyfile.reset();	
-	//std::cout << "private_key:" << keyfilecontent << " cert_content:" << certfilecontent;
 
-	//Step1 - Client Generate random g p pub_key
-	unsigned char client_content[512];
-	DH *dh_client = DH_new();
-	DH_generate_parameters_ex(dh_client,64,DH_GENERATOR_5,NULL);
-	DH_generate_key(dh_client);
+	rsa_key.reset(PEM_read_bio_RSAPrivateKey(shared_keyfile.get(), 0, 0, 0), RSA_free);
+	x509_cert.reset(PEM_read_bio_X509(shared_certfile.get(), 0, 0, 0), X509_free);
 
-	// 把 g,p, pubkey 传过去
-	std::string client_random_g, client_random_p, client_pub_key;
-	client_random_g.assign(reinterpret_cast<const char*>(client_content), BN_bn2bin(dh_client->g, client_content));
-	client_random_p.assign(reinterpret_cast<const char*>(client_content), BN_bn2bin(dh_client->p, client_content));
-	client_pub_key.assign(reinterpret_cast<const char*>(client_content), BN_bn2bin(dh_client->pub_key, client_content));
-	std::cout << "client public key:" << client_pub_key << std::endl;
+	std::cout << "private_key:" << keyfilecontent << " cert_content:" << certfilecontent;
+	std::cout << "load key cert ok." << std::endl;
 	
-	// Step 2 - Server Generate random number
-	DH* dh_server = DH_new();
-	unsigned char bin_key[512] = { 0 };
-	std::string server_pubkey;
+	dump_ca_info();
+}
 
-	// 生成随机数然后返回 m_dh->p ，让客户端去算共享密钥.
-	DH_generate_parameters_ex(dh_server, 64, DH_GENERATOR_5, NULL);
-	dh_server->g = BN_bin2bn((const unsigned char *)client_random_g.data(), static_cast<long>(client_random_g.length()), dh_server->g);
-	dh_server->p = BN_bin2bn((const unsigned char *)client_random_p.data(), static_cast<long>(client_random_p.length()), dh_server->p);
-
-	DH_generate_key(dh_server);
-	server_pubkey.assign(reinterpret_cast<const char*>(bin_key), BN_bn2bin(dh_server->pub_key, bin_key));
-	
-	// store client public key
-	std::vector<uint8_t> server_shared_key;
-	server_shared_key.resize(DH_size(dh_server));
-	BIGNUM* server_client_pubkey = BN_bin2bn((const unsigned char *)client_pub_key.data(), static_cast<long>(client_pub_key.length()), NULL);
-	DH_compute_key(&server_shared_key[0], server_client_pubkey, dh_server);
-	BN_free(server_client_pubkey);
-	DH_free(dh_server);
-	
-	std::string server_key;
-	char buf[16] = { 0 };
-	for (int i = 0; i < server_shared_key.size(); ++i)
+std::string RSA_private_encrypt(RSA * rsa, const std::string & from)
+{
+	std::string result;
+	const int keysize = RSA_size(rsa);
+	std::vector<unsigned char> block(keysize);
+	const int chunksize = keysize  - RSA_PKCS1_PADDING_SIZE;
+	int inputlen = from.length();
+	for(int i = 0 ; i < from.length(); i+= chunksize)
 	{
-		sprintf(buf, "%x%x", (server_shared_key[i] >> 4) & 0xf, server_shared_key[i] & 0xf);
-		server_key += buf;
+		int flen = std::min<int>(chunksize, inputlen - i);
+
+		std::fill(block.begin(),block.end(), 0);
+
+		auto resultsize = RSA_private_encrypt(
+			flen,
+			(uint8_t*) &from[i],
+			&block[0],
+			rsa,
+			RSA_PKCS1_PADDING
+		);
+		result.append((char*)block.data(), resultsize);
 	}
+	return result;
+}
 
-	std::cout << "server_key: " << server_key << std::endl;
-	
-	//step3 - Client calc server key
-	auto client_server_pubkey = BN_bin2bn((const unsigned char *) server_pubkey.data(), server_pubkey.length(), NULL);
-	std::vector<unsigned char> client_shared_key;
-	client_shared_key.resize(DH_size(dh_client));
-	// 密钥就算出来啦！
-	DH_compute_key(&client_shared_key[0], client_server_pubkey, dh_client);
-	BN_free(client_server_pubkey);
-
-    std::printf("key = 0x");
-    for (int i=0; i<DH_size(dh_client); ++i)
+std::string RSA_public_decrypt(RSA * rsa, const std::string & from)
+{
+	std::string result;
+	const int keysize = RSA_size(rsa);
+	std::vector<unsigned char> block(keysize);
+	std::cout << "start" << std::endl;
+	int inputlen = from.length();
+	std::cout << "start" << std::endl;
+	for(int i = 0 ; i < from.length(); i+= keysize)
 	{
-        std::printf("%x%x", (client_shared_key[i] >> 4) & 0xf, client_shared_key[i] & 0xf);
-    }
-    std::printf("\n");
-	DH_free(dh_client);
+		int flen = std::min(keysize, inputlen - i);
+
+		auto resultsize = RSA_public_decrypt(
+			flen,
+			(uint8_t*) &from[i],
+			&block[0],
+			rsa,
+			RSA_PKCS1_PADDING
+		);
+		std::cout << "ok" << std::endl;
+		result.append((char*)block.data(), resultsize);
+	}
+	return result;
+}
+
+int ca_process()
+{	
+	std::string rawdata("codecs test for openssl");
+	std::cout << "rawdata:" << rawdata<< std::endl;
+	std::string encrypt_data = RSA_private_encrypt(rsa_key.get(), rawdata);
+	std::cout << "after private_key encrypt_data:" << encrypt_data << std::endl;
 	
+	auto env_key = X509_get_pubkey(x509_cert.get());
+	auto user_rsa_pubkey = EVP_PKEY_get1_RSA(env_key);
+	EVP_PKEY_free(env_key);
+
+	std::string decrypt_data = RSA_public_decrypt(user_rsa_pubkey, encrypt_data);
+	RSA_free(user_rsa_pubkey);
+	std::cout << "after pubkey decrypt_data:" << decrypt_data << std::endl;
+	
+	return 0;
+}
+
+int ca_test()
+{
+	ca_load();
+	ca_process();
+	return 0;
+}
+
+int openssl_test()
+{	
+	OpenSSL_add_all_algorithms();
+	ca_test();
+	//dh_test();
 	
 	std::cout<< "openssl test end" << std::endl;
 	return 0;
